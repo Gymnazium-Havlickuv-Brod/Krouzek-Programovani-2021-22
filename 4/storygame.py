47857693b6afe165c7c6570805a66a0b09e7c3fc
adamferencz <43760358+adamferencz@users.noreply.github.com>
2021-10-14 18:38:15 +0200

    AUTO_SAVE_14.10.2021

--- 4/storygame.py
+++ 4/storygame.py
@@ -19,7 +19,8 @@ def ask(question):
 
 
 #pojmenujte hrace
-player["name"] = ask("Who are u?")
+# player["name"] = ask("Who are u?")
+player["name"] = "Adam"
 print("Hello, " + player["name"] + "!!")
 
 
@@ -31,15 +32,45 @@ shelf = [("jojo", 1),
         ("rocketlauncher", 10),
         ("lighter",1)]
 
+def print_item_list(item_list, phrase):
+    print(phrase)
+    print("------------------------------------")
+    for i in range(len(item_list)):
+        print(f"{str(i+1)}. - {item_list[i][0]} ({item_list[i][1]} kg)")
+    print("------------------------------------")
+    print("\n\n")
 
-# vytiskni itemy na policce
-print("On the shefl there are:")
-for i in range(len(shelf)):
-    print(f"{str(i+1)}. - {shelf[i][0]} ({shelf[i][1]} kg)")
+while True:
+    # Moje itemy
+    print_item_list(player["items"], "Your items are:")
 
-ask("What do u want?")
-# vyber jeden item podle cisla a pridej ho do batohu
+    # vytiskni itemy na policce
+    print_item_list(shelf, "On the shefl there are:")
 
+    # vyber jeden item podle cisla a pridej ho do batohu
+    wanted_index = ask("What do u want?")
+    wanted_index = int(wanted_index) - 1
+
+    wanted_item = shelf[wanted_index]
+
+    weight = 0
+    for item in player["items"]:
+        weight += item[1]
+
+
+    
+
+    if weight + wanted_item[1] > player["bag_cap"]:
+        print("This item is too heavy.")
+        print("Capacity is over: " + str(weight + wanted_item[1]) + "/" +str(player["bag_cap"]))
+        continue
+
+    # p5esun itemu na policku
+    shelf.remove(wanted_item)
+    player["items"].append(wanted_item)
+    print("Capacity: " + str(weight + wanted_item[1]) + "/" +str(player["bag_cap"]))
+
+    
 
 #zkontroluj ale jestli se tam vejde
 
